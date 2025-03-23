55b5773671c2c677ecbbc094fffac48ebc7697e9
johnwesly2002 <ujohnwesly8@gmail.com>
2025-03-23 12:33:40 +0530

    resolved the alignment issues

--- src/components/SuggestionsComponent/SuggestionsComponent.jsx
+++ src/components/SuggestionsComponent/SuggestionsComponent.jsx
@@ -5,16 +5,31 @@ export default function SuggestionsComponent({ query, suggestion }) {
 	return (
 		<div className="suggestion_container">
 			{suggestion?.AbstractText ? (
-				<h5 style={{color: 'black'}} key={suggestion?.AbstractText}>{suggestion?.AbstractText}</h5>
+				<h5 style={{ color: "black" }} key={suggestion?.AbstractText}>
+					{suggestion?.AbstractText}
+				</h5>
 			) : (
-				suggestion?.RelatedTopics?.map((topic, index) => (
-					topic.Text ? <div className="suggestion" key={index}>
-					{topic?.Icon?.URL ? <img src={`https://duckduckgo.com${topic?.Icon?.URL}`}  height={30} width={30} className="SuggestionImage" /> : <IoIosSearch size={20} color='black' />}
-					 <h5 style={{ color: "black" }} key={index}>
-						{topic.Text}
-					</h5> 
-					</div> : null
-				))
+				suggestion?.RelatedTopics?.map((topic, index) =>
+					topic.Text ? (
+						<div className="suggestion" key={index}>
+							{topic?.Icon?.URL ? (
+								<img
+									src={`https://duckduckgo.com${topic?.Icon?.URL}`}
+									height={30}
+									width={30}
+									className="SuggestionImage"
+								/>
+							) : (
+								<div className="SearchIcon">
+								<IoIosSearch size={30} color="black" />
+								</div>
+							)}
+							<h5 style={{ color: "black" }} key={index}>
+								{topic.Text}
+							</h5>
+						</div>
+					) : null
+				)
 			)}
 		</div>
 	);
